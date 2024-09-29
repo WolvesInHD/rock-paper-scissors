38a01fa8e412868261f80e3c4a31c6b82db67f74
WolvesInHD <wolvesinhd@gmail.com>
2024-09-29 13:48:39 -0400

    Create function to play 5 rounds & log results of
    the rounds. Assign counters for rounds and scores
    so they increment with each round. Call the
    playRound function 5x and display results of each
    one.

--- script.js
+++ script.js
@@ -7,25 +7,89 @@ Compare the selection of the user and the computer
 Assign win/loss counter
 */
 
-let computerOutput;
+/*Computer choice randomizaton */
 
-function randomSelection (min, max) {
-    return Math.round(Math.random() * (3 - 1) + 1)
+let computerOutput;
+let humanScore;
+let computerScore;
+let userChoice;
+let roundCount;
+
+computerScore = 0; 
+humanScore = 0;
+roundCount = 0;
+
+let result;
+function playRound(userChoice, computerOutput) {
+
+    roundCount++;
+
+    function randomSelection (min, max) {
+        return Math.round(Math.random() * (3 - 1) + 1)
+    }
+    
+    if (randomSelection(1, 3) == 1) {
+        computerOutput = "rock"
+    } else if (randomSelection (1, 3) == 2) {
+        computerOutput = "paper"
+    } else {
+        computerOutput = "scissors"
+    };
+
+    let userPrompt = prompt ("Select your choice! Rock, Paper, or Scissors?");
+    userChoice = userPrompt.toLowerCase();
+
+    if (userChoice == computerOutput) {
+        result = "It's a tie!";
+    } else if (userChoice == "rock" && computerOutput == "paper") {
+        computerScore++
+        result = "The computer wins this time!";
+    } else if (userChoice == "rock" && computerOutput == "scissors") {
+        humanScore++
+        result = "You win! Congrats!";
+    } else if (userChoice == "scissors" && computerOutput == "rock") {
+        computerScore++
+        result = "The computer wins this time!";
+    } else if (userChoice == "scissors" && computerOutput == "paper") {
+        humanScore++
+        result = "You win! Congrats!";
+    } else if (userChoice == "paper" && computerOutput == "scissors") {
+        computerScore++
+        result = "The computer wins this time!";
+    } else if (userChoice == "paper" && computerOutput == "rock") {
+        humanScore++
+        result = "You win! Congrats!";
+    } else {
+        result = "Wrong Input!"
+    }
 }
 
-if (randomSelection(1, 3) == 1) {
-    computerOutput = "Rock"
-} else if (randomSelection (1, 3) == 2) {
-    computerOutput = "Paper"
-} else {
-    computerOutput = "Scissors"
-};
+playRound(userChoice, computerOutput);
+console.log(result);
+console.log("Your score is " + humanScore);
+console.log("The computer score is " + computerScore);
+console.log("That was round " + roundCount);
 
-console.log(computerOutput)
+playRound(userChoice, computerOutput);
+console.log(result);
+console.log("Your score is " + humanScore);
+console.log("The computer score is " + computerScore);
+console.log("That was round " + roundCount);
 
+playRound(userChoice, computerOutput);
+console.log(result);
+console.log("Your score is " + humanScore);
+console.log("The computer score is " + computerScore);
+console.log("That was round " + roundCount);
 
-let userPrompt = prompt ("Select your choice! Rock, Paper, or Scissors?");
+playRound(userChoice, computerOutput);
+console.log(result);
+console.log("Your score is " + humanScore);
+console.log("The computer score is " + computerScore);
+console.log("That was round " + roundCount);
 
-let userChoice = userPrompt.toLowerCase();
-
-console.log(userChoice);
+playRound(userChoice, computerOutput);
+console.log(result);
+console.log("Your score is " + humanScore);
+console.log("The computer score is " + computerScore);
+console.log("That was round " + roundCount);
