26f2683c523e4a080f5453e99b49d5bd6e95525f
WolvesInHD <wolvesinhd@gmail.com>
2024-09-29 14:07:10 -0400

    Add for loop to execture playRound function until
    either humanScore or computerScore reaches 5. User
    was not able to see result of each round; added
    alerts after each round to notify user of the
    result of each round.

--- script.js
+++ script.js
@@ -21,13 +21,10 @@ roundCount = 0;
 
 let result;
 function playRound(userChoice, computerOutput) {
-
-    roundCount++;
-
+    
     function randomSelection (min, max) {
         return Math.round(Math.random() * (3 - 1) + 1)
     }
-    
     if (randomSelection(1, 3) == 1) {
         computerOutput = "rock"
     } else if (randomSelection (1, 3) == 2) {
@@ -41,55 +38,51 @@ function playRound(userChoice, computerOutput) {
 
     if (userChoice == computerOutput) {
         result = "It's a tie!";
+        alert(result);
     } else if (userChoice == "rock" && computerOutput == "paper") {
         computerScore++
+        roundCount++
         result = "The computer wins this time!";
+        alert(result);
     } else if (userChoice == "rock" && computerOutput == "scissors") {
         humanScore++
+        roundCount++
         result = "You win! Congrats!";
+        alert(result);
     } else if (userChoice == "scissors" && computerOutput == "rock") {
         computerScore++
+        roundCount++
         result = "The computer wins this time!";
+        alert(result);
     } else if (userChoice == "scissors" && computerOutput == "paper") {
         humanScore++
+        roundCount++
         result = "You win! Congrats!";
+        alert(result);
     } else if (userChoice == "paper" && computerOutput == "scissors") {
         computerScore++
+        roundCount++
         result = "The computer wins this time!";
+        alert(result);
     } else if (userChoice == "paper" && computerOutput == "rock") {
         humanScore++
+        roundCount++
         result = "You win! Congrats!";
+        alert(result);
     } else {
-        result = "Wrong Input!"
+        result = "Error! Try again!"
+        alert("Error! Try again!")
     }
 }
 
-playRound(userChoice, computerOutput);
-console.log(result);
-console.log("Your score is " + humanScore);
-console.log("The computer score is " + computerScore);
-console.log("That was round " + roundCount);
-
-playRound(userChoice, computerOutput);
-console.log(result);
-console.log("Your score is " + humanScore);
-console.log("The computer score is " + computerScore);
-console.log("That was round " + roundCount);
-
-playRound(userChoice, computerOutput);
-console.log(result);
-console.log("Your score is " + humanScore);
-console.log("The computer score is " + computerScore);
-console.log("That was round " + roundCount);
-
-playRound(userChoice, computerOutput);
-console.log(result);
-console.log("Your score is " + humanScore);
-console.log("The computer score is " + computerScore);
-console.log("That was round " + roundCount);
-
-playRound(userChoice, computerOutput);
-console.log(result);
-console.log("Your score is " + humanScore);
-console.log("The computer score is " + computerScore);
-console.log("That was round " + roundCount);
+for (let i = 0; i < 6; roundCount++) {
+    playRound(userChoice, computerOutput);
+    console.log(result);
+    console.log("Your score is " + humanScore);
+    console.log("The computer score is " + computerScore);
+    console.log("That was round " + roundCount);
+    if (humanScore == 5 || computerScore == 5) {
+        alert("GAME OVER!")
+        break;
+    }
+}
